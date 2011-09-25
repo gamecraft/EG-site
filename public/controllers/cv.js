55a2975801e8eca878f13801e23f7d869faf6b79
Mladen Gorchev <mladengorchev@krum-zaharievs-macbook.local>
2011-09-25 23:51:31 +0200

    More fixes

--- public/controllers/cv.js
+++ public/controllers/cv.js
@@ -2,28 +2,8 @@
     global.modules.CV = function(memberId){
         this.renderTo = function(target) {
             global.repo("TeamMember").get("/"+memberId, null, function(err, res){
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-                $(target).addClass("cvFrame");
-                global.view("/views/cv-frame.html")
-=======
                 $(target).addClass("cvFrame").removeClass("siteDefault");
                 global.view("/views/cv-body.html")
->>>>>>> e808c3779b6601858a897c0e13bc65113e470d81
-=======
-                $(target).addClass("cvFrame").removeClass("siteDefault");
-                global.view("/views/cv-body.html")
->>>>>>> e808c3779b6601858a897c0e13bc65113e470d81
-=======
-                $(target).addClass("cvFrame").removeClass("siteDefault");
-                global.view("/views/cv-body.html")
->>>>>>> e808c3779b6601858a897c0e13bc65113e470d81
-=======
-                $(target).addClass("cvFrame").removeClass("siteDefault");
-                global.view("/views/cv-body.html")
->>>>>>> e808c3779b6601858a897c0e13bc65113e470d81
                     .render(res.data, target, null, function(view){
                         
                     });
