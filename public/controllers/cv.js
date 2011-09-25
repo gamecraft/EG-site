d3e1e0df18889ac7d411f9ae4e0f6c53d079870f
Mladen Gorchev <mladengorchev@krum-zaharievs-macbook.local>
2011-09-25 23:45:23 +0200

    fix merged

--- public/controllers/cv.js
+++ public/controllers/cv.js
@@ -3,8 +3,13 @@
         this.renderTo = function(target) {
             global.repo("TeamMember").get("/"+memberId, null, function(err, res){
 <<<<<<< HEAD
+<<<<<<< HEAD
                 $(target).addClass("cvFrame");
                 global.view("/views/cv-frame.html")
+=======
+                $(target).addClass("cvFrame").removeClass("siteDefault");
+                global.view("/views/cv-body.html")
+>>>>>>> e808c3779b6601858a897c0e13bc65113e470d81
 =======
                 $(target).addClass("cvFrame").removeClass("siteDefault");
                 global.view("/views/cv-body.html")
