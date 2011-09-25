03763651d8c9d5442397b2608d7eb58bb1634ed0
Mladen Gorchev <mladengorchev@krum-zaharievs-macbook.local>
2011-09-25 23:49:54 +0200

    Fix-2

--- public/controllers/cv.js
+++ public/controllers/cv.js
@@ -5,8 +5,13 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
                 $(target).addClass("cvFrame");
                 global.view("/views/cv-frame.html")
+=======
+                $(target).addClass("cvFrame").removeClass("siteDefault");
+                global.view("/views/cv-body.html")
+>>>>>>> e808c3779b6601858a897c0e13bc65113e470d81
 =======
                 $(target).addClass("cvFrame").removeClass("siteDefault");
                 global.view("/views/cv-body.html")
