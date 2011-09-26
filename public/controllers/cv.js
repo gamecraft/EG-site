b3121f3d0d95430ebe96e86406c25780bc720036
Boris Filipov <obiwonn@gmail.com>
2011-09-27 02:11:04 +0300

    wired wito iamge field

--- public/controllers/cv.js
+++ public/controllers/cv.js
@@ -1,13 +1,22 @@
 (function(){
     global.modules.CV = function(memberId){
         var charts = new global.modules.StatsCharts();
+        var loadAllAchievements = function(next) {
+            global.repo("Achievement").list({},null,null,function(err, response) {
+    			next(response.data);
+    		});
+        };
+        var loadAllSkills = function(next) {
+            global.repo("Skill").list({},null,null,function(err, response) {
+    			next(response.data);
+    		});
+        };
         var loadTeamsData = function(next) {
     		var allTeams=[];
     		global.repo("Team").list({},null,null,function(err, response) {
     			allTeams=response.data;
     			next(allTeams);
     		});
-        	
     	};
     	
         var offsety = 760;
@@ -56,15 +65,29 @@
                 advanceCurrentOffsetY(target,imgHeight+spacey+30);
         }
         
-        var renderSkills = function(target, member) {
+        var renderSkills = function(target, member, skills) {
             $(".cvDefault .skillsHeader").css("top", offsety).css("left", 100);
             advanceCurrentOffsetY(target, 100);
+            for(var i = 0; i<member.skills.length; i++)
+                for(k = 0; k<skills.length; k++)
+                    if(member.skills[i].skillId == skills[k]._id) {
+                        member.skills[i].image = skills[k].image;
+                        break;
+                    }
+                        
             renderItems(target, member.skills, "http://github.com/gamecraft/EG-admin/raw/master/images/new_skill.png");
         };
         
-        var renderAchievements = function(target, member) {
+        var renderAchievements = function(target, member, achievements) {
             $(".cvDefault .achievementsHeader").css("top", offsety).css("left", 100);
             advanceCurrentOffsetY(target, 100);
+             for(var i = 0; i<member.achievements.length; i++)
+                for(k = 0; k<achievements.length; k++)
+                    if(member.achievements[i].achievementId == achievements[k]._id) {
+                        member.achievements[i].image = achievements[k].image;
+                        break;
+                    }
+                        
             renderItems(target, member.achievements, "http://github.com/gamecraft/EG-admin/raw/master/images/modifier_x2.png");
         };
     
@@ -73,40 +96,43 @@
                 $(target).addClass("cvDefault").removeClass("siteDefault");
                 
                 var member = res.data;
-                
-                loadTeamsData(function(allTeams){
-                    global.repo("Team").get("/"+res.data.teamId, null, function(err, teamRes) {
-                        var team = teamRes.data;
-                        var sorted = allTeams.sort(function(a, b) {
-                        	return b.totalPoints - a.totalPoints;
-                        });
-                        for(var teamScore = 1; teamScore<=sorted.length; teamScore++)
-                            if(sorted[teamScore-1]._id == res.data.teamId)
-                                break;
-                                
-                        global.view("/views/cv-body.html")
-                            .render({ name: member.name, teamName: team.name, teamScore: teamScore}, target, null, function(view){
-                                if(member.skills.length>0)
-                                    renderSkills($(".cvDefault"),member);
-                                else
-                                    $(".cvDefault .skillsHeader").hide();
-                                if(member.achievements.length>0)
-                                    renderAchievements($(".cvDefault"),member);
-                                else
-                                    $(".cvDefault .achievementsHeader").hide();
-                                
-                               	charts.totalPointsPerPhase.chartData(allTeams, function(chartLines, chartSeries){
-                               		charts.totalPointsPerPhase.plot("teamsPoints", "Отборни точки", 
-                               		                                chartLines, chartSeries, "Отборни точки",true);
-                               	});
-                               	charts.juryPointsPerPhasse.chartData(allTeams, function(chartLines, chartSeries){
-                               		charts.juryPointsPerPhasse.plot("juryPoints", "Отборни точки от журито",
-                               		                                 chartLines, chartSeries,"Отборни точки от журито",false);
-                               	});
-                               	charts.finalPointsPerTeam.chartData(allTeams,function(chartlines){
-                               		charts.finalPointsPerTeam.plot("skillLevels", "Общо класиране",chartlines);
-                               	});
+                loadAllAchievements(function(achievements) {
+                    loadAllSkills(function(skills) {
+                        loadTeamsData(function(allTeams){
+                            global.repo("Team").get("/"+res.data.teamId, null, function(err, teamRes) {
+                                var team = teamRes.data;
+                                var sorted = allTeams.sort(function(a, b) {
+                                	return b.totalPoints - a.totalPoints;
+                                });
+                                for(var teamScore = 1; teamScore<=sorted.length; teamScore++)
+                                    if(sorted[teamScore-1]._id == res.data.teamId)
+                                        break;
+                                        
+                                global.view("/views/cv-body.html")
+                                    .render({ name: member.name, teamName: team.name, teamScore: teamScore}, target, null, function(view){
+                                        if(member.skills.length>0)
+                                            renderSkills($(".cvDefault"),member,skills);
+                                        else
+                                            $(".cvDefault .skillsHeader").hide();
+                                        if(member.achievements.length>0)
+                                            renderAchievements($(".cvDefault"),member,achievements);
+                                        else
+                                            $(".cvDefault .achievementsHeader").hide();
+                                        
+                                       	charts.totalPointsPerPhase.chartData(allTeams, function(chartLines, chartSeries){
+                                       		charts.totalPointsPerPhase.plot("teamsPoints", "Отборни точки", 
+                                       		                                chartLines, chartSeries, "Отборни точки",true);
+                                       	});
+                                       	charts.juryPointsPerPhasse.chartData(allTeams, function(chartLines, chartSeries){
+                                       		charts.juryPointsPerPhasse.plot("juryPoints", "Отборни точки от журито",
+                                       		                                 chartLines, chartSeries,"Отборни точки от журито",false);
+                                       	});
+                                       	charts.finalPointsPerTeam.chartData(allTeams,function(chartlines){
+                                       		charts.finalPointsPerTeam.plot("skillLevels", "Общо класиране",chartlines);
+                                       	});
+                                    });
                             });
+                        });
                     });
                 });
             });
