bbf1440bb2ac87650fa4aeaed9d4f02c6730b3b6
Boris Filipov <obiwonn@gmail.com>
2011-09-27 12:12:26 +0300

    added teams listing

--- public/controllers/app.js
+++ public/controllers/app.js
@@ -2,6 +2,10 @@ $(document).ready(function(){
     global.repo.endpoint = "//178.79.173.17:3000";
     
     //setup crossroads
+    crossroads.addRoute('cv/teamsListing', function(memberId){
+        var listing = new global.modules.TeamsListing(memberId);
+        listing.renderTo($(document.body));        
+    });
     crossroads.addRoute('cv/{memberId}', function(memberId){
         var cv = new global.modules.CV(memberId);
         cv.renderTo($(document.body));        
